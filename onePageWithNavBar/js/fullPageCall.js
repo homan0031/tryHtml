e9f96d0ce2f8dcf673a16a6d2d73f70e8817546e
homan0031 <homan0031@gmail.com>
2016-12-13 17:41:47 +0800

    Add Sample
    Combine navigation bar with onepage screen

--- onePageWithNavBar/js/fullPageCall.js
+++ onePageWithNavBar/js/fullPageCall.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    sectionsColor: ['#81D4FA', '#29B6F6', '#039BE5', '#0277BD'],
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
     menu: '#menu',
+    paddingTop: '50px',
+    // scrollingSpeed: 1000
     autoScrolling: false
   });
 });
