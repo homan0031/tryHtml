eaadb95e3ef8ca716b589717cae67864ed38b98f
homan0031 <homan0031@gmail.com>
2016-12-14 17:40:23 +0800

    Update Website

--- onePageWithNavBar/js/fullPageCall.js
+++ onePageWithNavBar/js/fullPageCall.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor: ['#81D4FA', '#29B6F6', '#039BE5', '#0277BD'],
+    sectionsColor: ['#FFFFFF', '#29B6F6', '#039BE5', '#0277BD'],
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
     menu: '#menu',
-    paddingTop: '50px',
+    //paddingTop: '50px',
     // scrollingSpeed: 1000
     autoScrolling: false,
     fitToSection: false,
