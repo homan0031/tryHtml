1fa14ce693c0ef655fc1cb996911a1da72eb45cd
homan0031 <homan0031@gmail.com>
2017-03-11 11:36:08 +0800

    remove others

--- onePageWithNavBar/js/styleControl.js
+++
@@ -1,6 +0,0 @@
-//auto hide the nav bar for mobile version
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-        $(this).collapse('hide');
-    }
-});
